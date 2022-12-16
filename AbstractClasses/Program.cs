cf7e5919d27bbd8d655c75b56ab39bc4bbc62d18
evanmalherbe <evanmalherbe@gmail.com>
2022-12-16 08:37:39 +0200

    Add project files.

---
+++ AbstractClasses/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClasses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle();
+            circle.Draw();
+
+            Rectangle rectangle = new Rectangle();
+            rectangle.Draw();
+        }
+    }
+}
